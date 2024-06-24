6f655f5189d74e5c8f58e207ecca0161c83853c2
Dimitri97421 <dimitri.dg9@gmail.com>
2024-06-25 00:29:45 +0200

    Début section 1

--- mon-portfolio/src/App.js
+++ mon-portfolio/src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import './App.scss';
+import Header from './composants/Header'
+import Footer from './composants/Footer'
+import PhotoProfile from './img/moi.jpg'
 
 function App() {
   return (
     <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Header/>
+      <main className='main'>
+        <section className='accueil_section'>
+          <div className='presentation'>
+            <h1 className='typewriter'>
+              Bonjour, je suis Dimitri Gonthier
+            </h1>
+            <p className='typewriter'>
+              Je suis développeur web front end
+            </p>
+          </div>
+         <img src={PhotoProfile} alt='photo de moi' className='spin_animation'/>
+        </section>
+
+        <section className='projects_section'>
+        <h1>
+              Bonjour, je suis Dimitri Gonthier
+            </h1>
+        </section>
+
+        <section className='contact_section'>
+        <h1>
+              Bonjour, je suis Dimitri Gonthier
+            </h1>
+        </section>
+      </main>
+      <Footer/>
     </div>
   );
 }
